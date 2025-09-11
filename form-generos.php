cad07778bd0317c7fcabb792062544d612793805
emillyfr <107006896+emillyfr@users.noreply.github.com>
2025-09-11 01:56:41 -0400

    forms de cadastro e edição de gêneros

---
+++ form-generos.php
@@ -0,0 +1,90 @@
+<?php
+    session_start();
+    require_once('./conf/con_bd.php')
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atualizar Livro</title>
+    <meta name="description" content="BookStan - Editar livros">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="website icon" type="png" href="imagens/icon.png">
+</head>
+<body>
+    <header class="cabecalho">
+        <div>
+            <a class="titulo">BookStan</a>
+            <a class="subtitulo">Cadastro e Avaliações de Leitura</a>
+        </div>
+    </header>
+
+    <main class="principal">
+        <nav class="navegacao"></div>
+            <ul>
+                <li><a href="home.php">Página Inicial</a></li>
+                <li><a href="form-cadastro-livros.php">Novo Livro</a></li>
+                <li><a href="listar-generos.php">Gêneros Literários</a></li>
+                <li><a>Leituras Desejadas</a></li>
+                <li><a>Avaliações</a></li>
+            </ul>
+        </nav>
+        <div class="corpo">
+
+            <?php
+                $dados_generos = [
+                    'nome' => '',
+                    'descricao' => ''
+                ];
+                $action = "actions/generos/cadastrar.php"; 
+
+                if (isset($_GET['id'])) {
+                    $genero_id = mysqli_real_escape_string($con_bd, $_GET['id']);
+                    $sql = "SELECT nome, descricao FROM tb_generos WHERE id = $genero_id";
+                    $result = mysqli_query($con_bd, $sql);
+
+                    if (mysqli_num_rows($result) > 0) {
+                        $dados_generos = mysqli_fetch_assoc($result);
+                        $action = "actions/generos/atualizar.php"; 
+                    }
+                }
+            ?>
+
+
+
+            <form class="editar-genero" enctype="multipart/form-data" action="<?=$action?>" method="post">
+                <fieldset>
+                    <legend><?= (isset($_GET['id']) && is_numeric($_GET['id'])) ? "Editar Gênero" : "Cadastrar Gênero" ?></legend>
+                    
+                    <?php if (isset($genero_id)): ?>
+                        <input type="hidden" name="id" value="<?=$genero_id?>">
+                    <?php endif; ?>
+
+                    <label for="genero-nome">Gênero:</label>
+                    <input id="genero-nome" type="text" name="nome-genero" 
+                        value="<?=$dados_generos['nome']?>" required />
+                    
+                    <label for="descricao-genero">Descrição:</label>
+                    <input id="descricao-genero" type="text" name="descricao" 
+                        value="<?=$dados_generos['descricao']?>" required />
+
+                    <input type="submit" value="<?= (isset($_GET['id']) && is_numeric($_GET['id'])) ? "Editar" : "Cadastrar" ?>"/>
+                </fieldset>
+            </form>
+
+        </div>
+    </main>
+
+    <footer class="rodape">
+        <div class="conteudo-rodape">
+            <p>Trabalho</p>
+            <p>Tópicos especiais e Desenvolvimento de Sistemas I</p>
+            <p>Realizado por Giovanna Salvador e Emilly Rodrigues</p>
+            <p>&copy; 2024 BookStan. Todos os direitos reservados.</p>
+        </div>
+    </footer>
+
+</body>
+</html>
--- listar-generos.php
+++ listar-generos.php
@@ -40,7 +40,7 @@
         <div class="corpo">
             <div class="estante">
                 <div class="livro">
-                    <a href="form-cadastro-livros.php">
+                    <a href="form-generos.php?id=null">
                         <img class="adiciona" src="imagens/adicionar.png" alt="adicionar">
                     </a>
                 </div>
@@ -55,9 +55,9 @@
 
                         if($result && $result -> num_rows > 0) {
                             while ($row = $result->fetch_assoc()) {
-                                echo '<div class="genero">';
+                                echo '<div  class="genero">';
                                     echo '<p>' . htmlspecialchars($row['nome']) . '</p>';
-                                    echo '<a href="listar-livros.php?id='.$row['id'].'" class="acao visualizar">
+                                    echo '<a href="form-generos.php?id='.$row['id'].'" class="acao visualizar">
                                         <p>' . htmlspecialchars($row['descricao']) . '</p>
                                     </a>';
                                     echo '<form action="actions/generos/deletar.php" method="POST">
@@ -65,7 +65,7 @@
                                                 <img src="imagens/excluir.png" alt="excluir">
                                         </button>
                                      </form>';
-                                    echo '<a href="form-atualiza-genero.php?id='.$row['id'].'" class="acao editar">Editar</a>';
+                                    echo '<a href="form-generos.php?id='.$row['id'].'" class="acao editar">Editar</a>';
                                 echo '</div>';
                             }
                         }
